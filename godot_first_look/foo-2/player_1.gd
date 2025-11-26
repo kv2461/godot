10f19ff6ed917ce65c38070120b984cd295090fb
Kevin Vicencio <kevinarnoldcode@gmail.com>
2025-11-25 18:07:34 -0800

    did a tutorial less run of what ir emembered and then some, good practice

---
+++ godot_first_look/foo-2/player_1.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+@export var speed = 400
+var screen_size
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		if abs(velocity.x) > 0:
+			$AnimatedSprite2D.play("walk_horizontal")
+		if velocity.y < 0:
+			$AnimatedSprite2D.play("walk_vertical")
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
