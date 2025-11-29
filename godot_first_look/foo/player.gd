3ef86a81cd27446e65df0802638074f0597ba3e7
kv2461 <kevinarnoldcode@gmail.com>
2025-11-29 13:13:50 -0800

    Update animation orientation based on direction

--- godot_first_look/foo/player.gd
+++ godot_first_look/foo/player.gd
@@ -19,10 +19,13 @@ func _process(delta):
 		
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		if velocity.y < 0:
-			$AnimatedSprite2D.play("up")
-		else:
+		if velocity.x != 0:
 			$AnimatedSprite2D.play("walk")
+			$AnimatedSprite2D.flip_v = false
+			$AnimatedSprite2D.flip_h = velocity.x < 0 
+		elif velocity.y != 0:
+			$AnimatedSprite2D.play("up")
+			$AnimatedSprite2D.flip_v = velocity.y > 0
 	else:
 		$AnimatedSprite2D.stop()
 		
