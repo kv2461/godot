3cb49d3039f4633060c171c8aa23a4adcd34d6fa
Kevin Vicencio <kevinarnoldcode@gmail.com>
2025-12-05 16:49:29 -0800

    Update script to randomly play 1 of the 3 animations so that we essentially have 3 random mob types

--- godot_first_look/foo/mob.gd
+++ godot_first_look/foo/mob.gd
@@ -1,10 +1,10 @@
 extends RigidBody2D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
+	$AnimatedSprite2D.animation = mob_types.pick_random()
+	$AnimatedSprite2D.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
