90e9bbfaf0197c23907ef4c8dc33c6e8ce2f894e
Kevin Vicencio <kevinarnoldcode@gmail.com>
2025-12-07 15:03:48 -0800

    Update mob scene to play animation sprites

--- godot_first_look/foo/mob.gd
+++ godot_first_look/foo/mob.gd
@@ -4,6 +4,7 @@ extends RigidBody2D
 func _ready() -> void:
 	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
 	$AnimatedSprite2D.animation = mob_types.pick_random()
+	$AnimatedSprite2D.play()
 	
 
 func _process(delta: float) -> void:
