a542e2f608af26d37b78f576a07b127fa1139d67
Kevin Vicencio <kevinarnoldcode@gmail.com>
2025-12-08 20:26:33 -0800

    Make game over function that shows game over, and restarts to original start menu

--- godot_first_look/foo/hud.gd
+++ godot_first_look/foo/hud.gd
@@ -15,3 +15,12 @@ func show_message(text):
 	$Message.text = text
 	$Message.show()
 	$MessageTimer.start()
+	
+func show_game_over():
+	show_message("Game Over")
+	await $MessageTimer.timeout
+	
+	$Message.text = "Dodge the Creeps!"
+	$Message.show()
+	await get_tree().create_timer(1.0).timeout
+	$StartButton.show()
